46cda6b8d3c3a1104e89c9fe41ed7d5a741b4df6
indrausfiyadi <indrausfiyadi@gmail.com>
2018-07-13 10:50:45 +0700

    create dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
